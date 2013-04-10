0722bb78eaa91b4a1b23d573fab5a31099b64eda
Ken Schwencke <schwank@gmail.com>
2013-04-10 01:24:21 -0700

    formatting

--- src/index.cljs
+++ src/index.cljs
@@ -1,6 +1,5 @@
 (ns currentstatus-fm
-  (:require 
-   [dommy.template :as template])
+  (:require [dommy.template :as template])
   (:use [jayq.core :only [$ append]]
         [clojure.string :only [split]])
   (:use-macros [jayq.macros :only [let-ajax]]))
@@ -9,8 +8,7 @@
 
 (def container ($ :div#container))
 
-(defn get-url-for-item
-  [item]
+(defn get-url-for-item [item]
   (.-expanded_url (first (.-urls (.-entities item)))))
 
 (defn get-youtube-id [item]
@@ -19,21 +17,19 @@
 (defn get-youtube-embed [item]
   (template/node
    [:iframe 
-    {
-     :width "560"
+    {:width "560"
      :height "315"
      :src (str "http://www.youtube.com/embed/" (get-youtube-id item))
      :frameborder "0"
-     }
-    ]))
+     }]))
 
 
-(let-ajax [data {:url "http://search.twitter.com/search.json?q=current%20status%20youtu.be&include_entities=true"
-              :dataType :jsonp}]
-          (append
-           container
-           (template/node
-            [:ul
-             (for [r (.-results data)]
-               [:li (get-youtube-embed r)])])))
-
+(let-ajax [data 
+           {:url "http://search.twitter.com/search.json?q=current%20status%20youtu.be&include_entities=true"
+            :dataType :jsonp}]
+  (append
+     container
+     (template/node
+      [:ul
+       (for [r (.-results data)]
+         [:li (get-youtube-embed r)])])))
